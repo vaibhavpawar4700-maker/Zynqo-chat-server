fef37bd46fc5f571912a24bcbeaf8b49c59c4646
vaibhavpawar4700-maker <vaibhavpawar4700@gmail.com>
2026-03-01 11:20:44 +0530

    Add files via upload

---
+++ server6.py
@@ -0,0 +1,51 @@
+import asyncio
+import websockets
+import json
+
+clients = {}   # username : websocket
+
+
+async def handler(websocket):
+    try:
+        username = await websocket.recv()
+        clients[username] = websocket
+        print(f"{username} connected")
+
+        await send_users()
+
+        async for message in websocket:
+            data = json.loads(message)
+            target = data["to"]
+            text = data["msg"]
+            sender = data["from"]
+
+            if target in clients:
+                await clients[target].send(
+                    json.dumps({"from": sender, "msg": text})
+                )
+
+    except:
+        pass
+    finally:
+        if username in clients:
+            del clients[username]
+            await send_users()
+
+
+async def send_users():
+    user_list = json.dumps({
+        "type": "users",
+        "list": list(clients.keys())
+    })
+
+    for ws in clients.values():
+        await ws.send(user_list)
+
+
+async def main():
+    async with websockets.serve(handler, "0.0.0.0", 10000):
+        print("Server started")
+        await asyncio.Future()
+
+
+asyncio.run(main())
